cee3e2b48906518ee5ecea3d729cd84c39b32144
Will Gueble <will.gueble@viget.com>
2013-07-02 13:26:19 -0600

    Commit Base App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpsGame::Application.config.secret_token = '3335320fbfad12eca09c164e39caf3aa80e349bcd865bed8d3887083dc2b828307cd400b0dd71afecc39c194f2f0179602de781ba2a753bc1f77f64300602287'
